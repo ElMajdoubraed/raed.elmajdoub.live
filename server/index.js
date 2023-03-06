bd3bac12ebe25f82d44f3f71f02910c207fe5ce8
raed elmajdoub <raed@eng.leadshook.com>
2023-03-06 20:41:08 +0100

    add validemail functions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const serverless = require("serverless-http");
 const express = require("express");
 const DaynamoSDK = require('./aws/dynamo.sdk');
 const LocalMail = require('./services/mailer.service');
+const VerifEmail = require('./services/verif.mail');
 var bodyParser = require('body-parser');
 const app = express();
 
@@ -10,26 +11,26 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.get("/", (req, res, next) => {
-  return res.status(200).json({
-    message: "deployed from github actions",
-  });
-});
-
 app.post("/send", async (req, res, next) => {
   const { username, email, message, attemps = 0 } = req.body;
-  console.log(username, email, message);
-
-  const result = await DaynamoSDK.saveIntoDynamoDB(username, email, message, attemps);
-  if (result.code === 200) {
-    await LocalMail.sendLocalMail(username, email, message );
+  if (VerifEmail.isValidEmail__(email) !== 'isValidEmail') {
+    return res.json({
+      code: 400,
+      status: 'failed',
+      details: 'Invalid email',
+    });
+  } else {
+    const result = await DaynamoSDK.saveIntoDynamoDB(username, email, message, attemps);
+    if (result.code === 200) {
+      await LocalMail.sendLocalMail(username, email, message );
+    }
+    return res.json({
+      code: result.code,
+      status: result.status,
+      details: result.details,
+    });
   }
-  return res.json({
-    code: result.code,
-    status: result.status,
-    details: result.details,
-  });
+
 });
 
 app.use((req, res, next) => {
