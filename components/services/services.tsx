83c441162e1bb77f4e9c6a8b560e005a8a642076
raed elmajdoub <raed@eng.leadshook.com>
2023-03-10 22:31:23 +0100

    addd experience

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -1,10 +1,20 @@
 import { BiCheck } from "react-icons/bi"
 import { Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
+import { useEffect, useState } from "react";
+import experiences from "./experience.json"
 import 'swiper/css';
 import 'swiper/css/pagination';
 export default function Services() {
+    const experience_array: any = experiences; 
+    const [slideNum, setSlideNum] = useState(1)
+    useEffect(() => {
+        var w = window.innerWidth;
+        if (w > 1024) {
+            setSlideNum(3)
+        }
+
+    })
   return (
     <section id="services">
         <h5>Experience i have gained</h5>
@@ -13,46 +23,31 @@ export default function Services() {
         <Swiper
                 modules={[Pagination]}
                 spaceBetween={40}
-                slidesPerView={3}
+                slidesPerView={slideNum}
                 navigation
                 pagination={{ clickable: true }}
                 className="container services__container"
         >
                 
-        <SwiperSlide className="service">
-            <div className="service__head">
-                <h3>UI/UX design</h3>
-                <p className="service__description">khdemet maa kadha kadha</p>
-            </div>
-
-            <ul className="service__list">
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-            </ul>
-        </SwiperSlide>
-
-        <SwiperSlide className="service">
-            <div className="service__head">
-                <h3>UI/UX design</h3>
-            </div>
-
-            <ul className="service__list">
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-            </ul>
-        </SwiperSlide>
+            {
+                experience_array.map((experience: any, index: number) => 
+                    <SwiperSlide key={'experience_key_' + index} className="service">
+                        <div className="service__head">
+                            <h3>{experience.title}</h3>
+                            <p className="service__description">{experience.description}</p>
+                        </div>
+                        <ul className="service__list">
+                        {
+                            experience.tasks.map((task: string, i: number) =>
+                            <li>
+                                <BiCheck className="service__list__icon"/>
+                                <p>{task}</p>
+                            </li> 
+                            )
+                        }
+                        </ul>        
+                    </SwiperSlide>)
+            }
         
 
         </Swiper>
