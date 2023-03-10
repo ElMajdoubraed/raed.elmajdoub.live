6658c4a7937ded1ba3dbb4e47b2d754f9b94dc51
raed elmajdoub <raed@eng.leadshook.com>
2023-03-10 20:53:19 +0100

    add styling

--- components/services/services.tsx
+++ components/services/services.tsx
@@ -1,12 +1,43 @@
 import { BiCheck } from "react-icons/bi"
+import { Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 export default function Services() {
   return (
     <section id="services">
         <h5>Experience i have gained</h5>
         <h2>Experience</h2>
 
-        <div className="container services__container">
-        <article className="service">
+        <Swiper
+                modules={[Pagination]}
+                spaceBetween={40}
+                slidesPerView={3}
+                navigation
+                pagination={{ clickable: true }}
+                className="container services__container"
+        >
+                
+        <SwiperSlide className="service">
+            <div className="service__head">
+                <h3>UI/UX design</h3>
+                <p className="service__description">khdemet maa kadha kadha</p>
+            </div>
+
+            <ul className="service__list">
+                <li>
+                    <BiCheck className="service__list__icon"/>
+                    <p>task name</p>
+                </li>
+                <li>
+                    <BiCheck className="service__list__icon"/>
+                    <p>task name</p>
+                </li>
+            </ul>
+        </SwiperSlide>
+
+        <SwiperSlide className="service">
             <div className="service__head">
                 <h3>UI/UX design</h3>
             </div>
@@ -21,26 +52,10 @@ export default function Services() {
                     <p>task name</p>
                 </li>
             </ul>
-        </article>
+        </SwiperSlide>
+        
 
-        <article className="service">
-            <div className="service__head">
-            <h3>UI/UX design</h3>
-            </div>
-
-            <ul className="service__list">
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-                <li>
-                    <BiCheck className="service__list__icon"/>
-                    <p>task name</p>
-                </li>
-            </ul>
-        </article>
-
-        </div>
+        </Swiper>
     </section>
   )
 }
