e6f59379652d99975593e87af29063cf88460a9e
raed <raed@eng.leadshook.com>
2023-03-19 04:59:58 +0100

    changes styling

--- components/experience/experience.tsx
+++ components/experience/experience.tsx
@@ -2,7 +2,16 @@ import { BsPatchCheckFill } from "react-icons/bs";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import skills from './skills.json'
 import { motion } from 'framer-motion'
+import { useEffect, useState } from "react";
 export default function Experience() {
+    const [isMobile, setIsMobile] = useState(false)
+    useEffect(() => {
+        var w = window.innerWidth;
+        if (w < 1024) {
+            setIsMobile(true)
+        }
+
+    })
     const skillarray: any = skills
     return (
         <motion.section 
@@ -30,25 +39,47 @@ export default function Experience() {
                 initial = {{ opacity: 0 }}
                 whileInView = {{ opacity: 1}}
                 transition = {{ duration: 2 }}
-            >            
+            >    
                 {
-                    skillarray.map((skill: any, index: number) => 
-                    <div key={'experience_' + index} className="experience__frontend">
-                            <h3>{skill.title}</h3>
-                            {
-                                skill.skills.map((e: string, i: number) =>  
-                                <div key={'skill_' + i} className="experience__content">
-                                    <article className="experience__details">
-                                        <BsPatchCheckFill className="experience__details__icon " />
-                                        <h4>{e}</h4>
-                                    </article>
-                                </div>
-                                )
-                            }
-                        
-                    </div>
-                    )
-                }
+                    !isMobile ? <>
+                    {
+                        skillarray.map((skill: any, index: number) => 
+                        <div key={'experience_' + index} className="experience__frontend">
+                                <h3>{skill.title}</h3>
+                                {
+                                    skill.skills.map((e: string, i: number) =>  
+                                    <div key={'skill_' + i} className="experience__content">
+                                        <article className="experience__details">
+                                            <BsPatchCheckFill className="experience__details__icon " />
+                                            <h4>{e}</h4>
+                                        </article>
+                                    </div>
+                                    )
+                                }
+                            
+                        </div>
+                        )
+                    } </>: <>
+                    {
+                        skillarray.map((skill: any, index: number) => 
+                        <details key={'experience_' + index} className="experience__frontend">
+                                <summary><h3>{skill.title}</h3></summary>
+                                {
+                                    skill.skills.map((e: string, i: number) =>  
+                                    <div key={'skill_' + i} className="experience__content">
+                                        <article className="experience__details">
+                                            <BsPatchCheckFill className="experience__details__icon " />
+                                            <h4>{e}</h4>
+                                        </article>
+                                    </div>
+                                    )
+                                }
+                            
+                        </details>
+                        )
+                    }
+                    </>
+                }        
             </motion.div>
         </motion.section>
     )
