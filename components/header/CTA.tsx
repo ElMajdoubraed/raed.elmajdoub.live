ad1f0cbb64a7c40ee29e0c3de5499be1f2366be5
raed <raed@eng.leadshook.com>
2023-03-19 15:27:14 +0100

    try to fix animation

--- components/header/CTA.tsx
+++ components/header/CTA.tsx
@@ -2,7 +2,7 @@
 const CTA = () => {
     return (
         <div className="cta">
-            <a href={''} download className="btn">Download CV</a>
+            <a href='' download aria-disabled={true} className="btn">Download CV</a>
             <a href="#contact" className="btn btn-primary">Let's talk</a>
         </div>
     )
