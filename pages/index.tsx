720bcf3a6af65b29d1a05275d59e86e077c4006b
raed <raed@eng.leadshook.com>
2023-06-16 17:03:20 +0100

    remove NoIndex robot

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,6 @@ export default function Index() {
   return (
     <>
       <Head>
-        <meta name="robots" content="noindex" />
-        <meta name="googlebot" content="noindex" />
         <link rel="apple-touch-icon" href="/icon-192x192.png" />
         <meta name="google" content="notranslate" />
         <meta
