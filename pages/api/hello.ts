8eff097b8c42b4699fb90dcd859bfaa9135077cb
raed <raed@eng.leadshook.com>
2023-06-22 21:18:31 +0100

    change api hello message

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
+  message: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "Raed Elmajdoub" });
+  res.status(200).json({ message: "Raed Elmajdoub - API" });
 }
